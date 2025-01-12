a8760a44ce7ef6a6310745a2c916353223685224
eonist <30n1st@gmail.com>
2025-01-12 19:45:48 +0100

    Add testUpdateTimeWithInvalidURL unit test via o1

--- Tests/NetTimeTests/UnitTests.swift
+++ Tests/NetTimeTests/UnitTests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import NetTime
 
 final class UnitTests: XCTestCase {
+    // fixme: add doc
     func testDateFormatterParsing() {
         let dateString = "Sun, 12 Jan 2025 17:44:00 GMT"
         let formatter = Date.formatter
@@ -19,5 +20,20 @@ final class UnitTests: XCTestCase {
         XCTAssertEqual(components.hour, 17)
         XCTAssertEqual(components.minute, 44)
         XCTAssertEqual(components.second, 0)
+    }
+    // Unit Tests for Error Conditions
+    // Add unit tests that cover error scenarios, such as network failures or date parsing failures, to ensure your error handling works as expected.
+    // fixme: add doc
+    func testUpdateTimeWithInvalidURL() {
+        let expectation = self.expectation(description: "Completion handler invoked")
+        Date.updateTime(with: URL(string: "invalid_url")!) { result in
+            if case .failure(let error) = result {
+                XCTAssertNotNil(error)
+            } else {
+                XCTFail("Expected failure when using an invalid URL")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
     }
 }
