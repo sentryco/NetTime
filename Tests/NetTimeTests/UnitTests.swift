b1bc084b7b569a0563dd255dc97b976c901e85f3
eonist <30n1st@gmail.com>
2025-01-12 20:25:13 +0100

    Update UnitTests.swift

--- Tests/NetTimeTests/UnitTests.swift
+++ Tests/NetTimeTests/UnitTests.swift
@@ -2,7 +2,9 @@ import XCTest
 @testable import NetTime
 
 final class UnitTests: XCTestCase {
-    // fixme: add doc
+   /**
+    * - Fixme: ⚠️️ add doc
+    */
     func testDateFormatterParsing() {
         let dateString = "Sun, 12 Jan 2025 17:44:00 GMT"
         let formatter = Date.formatter
@@ -21,9 +23,11 @@ final class UnitTests: XCTestCase {
         XCTAssertEqual(components.minute, 44)
         XCTAssertEqual(components.second, 0)
     }
-    // Unit Tests for Error Conditions
-    // Add unit tests that cover error scenarios, such as network failures or date parsing failures, to ensure your error handling works as expected.
-    // fixme: add doc
+   /**
+    * Unit Tests for Error Conditions
+    * Add unit tests that cover error scenarios, such as network failures or date parsing failures, to ensure your error handling works as expected.
+    * - Fixme: ⚠️️ add doc
+    */
     func testUpdateTimeWithInvalidURL() {
         let expectation = self.expectation(description: "Completion handler invoked")
         Date.updateTime(with: URL(string: "invalid_url")!) { result in
@@ -36,4 +40,18 @@ final class UnitTests: XCTestCase {
         }
         waitForExpectations(timeout: 5, handler: nil)
     }
+   /**
+    * - Fixme: ⚠️️ add doc
+    */
+    func testTimeGapCalculation() {
+         // Set a known timeGap
+         Date.timeGap = 60 // 1 minute
+         let adjustedServerTime = Date.serverTime
+         let expectedTime = Date().addingTimeInterval(60)
+         XCTAssertEqual(
+             adjustedServerTime.timeIntervalSince1970,
+             expectedTime.timeIntervalSince1970,
+             accuracy: 0.1
+         )
+     }
 }
